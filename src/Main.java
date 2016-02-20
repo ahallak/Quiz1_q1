f25ffa35b9921305497f5a45c10ed2178608e58c
Amjed Hallak <ahallak@udel.edu>
2016-02-20 15:09:54 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) { 
-		
+
 		int touchdowns;
 		double totalYards;
 		int interceptions;
@@ -13,9 +13,9 @@ public class Main{
 		double calcC;
 		double calcD;
 		double passerRating;
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter number of touchdowns");
 		touchdowns = input.nextInt();
 		System.out.println("Enter total yards");
@@ -26,12 +26,12 @@ public class Main{
 		completions = input.nextInt();
 		System.out.println("Enter attempted passes");
 		attemptedPasses = input.nextInt();
-		
+
 		calcA = ((completions/attemptedPasses) - 0.3) * -5;
 		calcB = ((totalYards/attemptedPasses) - 3) * .25;
 		calcC = ((touchdowns/attemptedPasses) * 20);
 		calcD = 2.375 - ((interceptions/attemptedPasses) * 25);
-		
+
 		passerRating = ((calcA + calcB + calcC + calcD)/6)*100;
 		System.out.println(calcA + " " + calcB + " " + calcC + " " + calcD);
 		System.out.println("Passer's rating: " + passerRating);
